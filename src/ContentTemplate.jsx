66cad75f6d5ec50603c3c4288f54c385cab7bcdc
Blaž Mandl <blaz.mandl@gmail.com>
2020-04-02 20:15:18 +0200

    Moved container class from index.html to template component

--- src/ContentTemplate.jsx
+++ src/ContentTemplate.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.scss';
 
 export default (props) => (
-    <div>
+    <div class="container shadow p-5 mt-6 mb-5 rounded-lg border">
         <div class="title-center-box"><h1 id="title" class="container-title rounded-lg shadow">{props.title}</h1></div>      
         {props.content}
     </div>
