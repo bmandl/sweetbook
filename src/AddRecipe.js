981f5ffd53a94a700f8f12c246bf4c1f58fa976d
Blaž Mandl <blaz.mandl@gmail.com>
2020-04-03 15:24:05 +0200

    Added react-bootstrap
    - Added react-bootstrap to project
    - changed jsx in AddRecipe to react-bootstrap style
    - Added function to dinamicly create array of FormGroup's for future adding
    - Added 20px bottom-margin to h2 element in section.hr class

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -1,81 +1,112 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+function createFormGroupArray() {
+
+    let groupParameters = Array.from(arguments);
+    let groupArray = [];
+    groupParameters.forEach(obj => {
+        groupArray.push(
+            <>
+                <Form.Group controlId={obj.id}>
+                    <Form.Label>{obj.label}</Form.Label>
+                    <Form.Control as={obj.control.as} type={obj.control.type} />
+                </Form.Group>
+                {obj.button &&
+                    <Button name={obj.button.name} variant={obj.button.variant}>
+                        {obj.button.text}
+                    </Button>
+                }
+            </>
+        )
+    })
+
+    return groupArray;
+}
 
 const AddRecipe = () => {
     return (
         <>
-            <div class="form-group">
-                <label for="title">Title:</label>
-                <input id="title" class="form-control" type="text" name="title" />
-            </div>
-
-            <div class="form-group">
-                <label for="brief">Brief description:</label>
-                <textarea id="brief" class="form-control" name="brief"></textarea>
-            </div>
+            {createFormGroupArray(
+                {
+                    id: "title",
+                    label: "Title:",
+                    control: { as: "input", type: "text" }
+                },
+                {
+                    id: "brief",
+                    label: "Brief description:",
+                    control: { as: "textarea" }
+                })}
 
             <section class="hr">
                 <h2>Ingredients</h2>
-                <div class="form-group">
-                    <label for="name">Name:</label>
-                    <input id="ingredient" class="form-control" type="text" name="ingredient" />
-                </div>
-
-                <div class="form-group">
-                    <label for="quantity">Quantity:</label>
-                    <input id="quantity" class="form-control" type="text" name="quantity" />
-                </div>
-
-                <div class="form-group">
-                    <label for="unit">Unit:</label>
-                    <select id="unit" class="form-control" name="unit">
-                    </select>
-                </div>
-                <button name="addIngredient" class="btn btn-primary">Add ingredient</button>
+                {createFormGroupArray(
+                    {
+                        id: "name",
+                        label: "Name:",
+                        control: { as: "input", type: "text" }
+                    },
+                    {
+                        id: "quantity",
+                        label: "Quantity:",
+                        control: { as: "input", type: "text" }
+                    },
+                    {
+                        id: "unit",
+                        label: "Unit:",
+                        control: { as: "select" },
+                        button: { name: "addIngredient", variant: "primary", text: "Add ingredient" }
+                    }
+                )}
             </section>
 
             <section class="hr">
-                <h2>Preparation</h2>
-                <div class="form-group">
-                    <label for="step">Step:</label>
-                    <input id="step" class="form-control" type="text" name="step" />
-                </div>
-                <button name="addStep" class="btn btn-primary">Add step</button>
+                <h2>Preparation</h2>                
+                <Form.Group controlId="step">
+                    <Form.Label>Step:</Form.Label>
+                    <Form.Control as="input" type="text" />                    
+                </Form.Group>
+                <Button name="addStep" variant="primary">Add step</Button>
             </section>
 
             <section class="hr">
                 <h2>Details</h2>
-                <div class="form-group">
-                    <label for="type">Type:</label>
-                    <input id="type" class="form-control" type="text" name="type" />
-                </div>
-
-                <div class="form-group">
-                    <label for="main">Main ingredient(s):</label>
-                    <input id="main" class="form-control" type="text" name="main" />
-                </div>
-
-                <div class="form-group">
-                    <label for="occasion">Season/Ocasion:</label>
-                    <input id="occasion" class="form-control" type="text" name="occasion" />
-                </div>
-
-                <div class="form-group">
-                    <label for="method">Preparation method:</label>
-                    <input id="method" class="form-control" type="text" name="method" />
-                </div>
-
-                <div class="form-group">
-                    <label for="diet">Dietary consideration:</label>
-                    <input id="diet" class="form-control" type="text" name="diet" />
-                </div>
+                {createFormGroupArray(
+                    {
+                        id: "type",
+                        label: "Type:",
+                        control: { as: "input", type: "text" }
+                    },
+                    {
+                        id: "main",
+                        label: "Main ingredient(s):",
+                        control: { as: "input", type: "text" }
+                    },
+                    {
+                        id: "occasion",
+                        label: "Season/Ocasion:",
+                        control: { as: "input", type: "text" }
+                    },
+                    {
+                        id: "method",
+                        label: "Preparation method:",
+                        control: { as: "input", type: "text" }
+                    },
+                    {
+                        id: "diet",
+                        label: "Dietary consideration:",
+                        control: { as: "input", type: "text" }
+                    }
+                )}
             </section>
 
             <section class="hr">
                 <h2>Notes</h2>
-                <div class="form-group">
-                    <label for="notes"></label>
-                    <textarea id="notes" class="form-control" name="notes"></textarea>
-                </div>
+                <Form.Group>
+                    <Form.Label for="notes"></Form.Label>
+                    <Form.Control as="textarea" id="notes" name="notes"></Form.Control>
+                </Form.Group>
             </section>
         </>
     )
